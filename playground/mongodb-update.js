e239f8861be8031a8f5cefe5054303fc84faf729
Jose Mendoza <josemendoza@Joses-MacBook-Pro.local>
2017-11-29 19:09:30 -0700

    added basic API functionality

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -6,5 +6,12 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,db)=>{
 	}
 	console.log('Connected to MongoDB server');
 
-	
+	db.collection('Todos').findOneAndUpdate(
+		{_id:new ObjectID('5a1f3fb2e2720a124e67d1d8')},
+		{$set:{
+			completed:true
+		}},
+		{returnOriginal:false}).then((result)=>{
+		console.log(result);
+	});
 });
