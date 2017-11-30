7024b71320f0ded081502d7fa0ccb1c9ed21b094
Jose Mendoza <josemendoza@Joses-MacBook-Pro.local>
2017-11-30 08:16:58 -0700

    add GET routes

--- server/server.js
+++ server/server.js
@@ -10,6 +10,10 @@ var app = express();
 //middleware
 app.use(bodyParser.json());
 
+
+//Define server methods.
+
+//Define input.
 app.post('/todos', (req,res)=>{
 	var todo = new Todo({
 		text: req.body.text
@@ -17,9 +21,19 @@ app.post('/todos', (req,res)=>{
 	todo.save().then((doc)=>{
 		res.send(doc);
 	}, (e)=>{ 
-		res.status(400).send(e);});
+		res.status(400).send(e);
+	});
 });
 
+//Define output.
+app.get('/todos', (req,res)=>{
+	Todo.find().then((todos)=>{
+		res.send({todos});
+	}, (e)=>{res.status(400).send(e);});
+});
+
+
+//Start app and add to exports (header file).
 app.listen(3000, () => {
 	console.log('Server started on port 3000.');
 });
