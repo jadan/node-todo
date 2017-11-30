3a200e427d76dad8e748f7e1aea3c2867588f459
Jose Mendoza <josemendoza@Joses-MacBook-Pro.local>
2017-11-30 08:31:38 -0700

    tests

--- server/server.js
+++ server/server.js
@@ -27,9 +27,13 @@ app.post('/todos', (req,res)=>{
 
 //Define output.
 app.get('/todos', (req,res)=>{
-	Todo.find().then((todos)=>{
-		res.send({todos});
-	}, (e)=>{res.status(400).send(e);});
+	Todo.find().then(
+		(todos)=>{
+			res.send({todos});
+		}, 
+		(e)=>{
+			res.status(400).send(e);
+		});
 });
 
 
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,13 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+
+const todos = [{text:'First test todo'}, {text:'Second test todo'}];
+
 beforeEach((done)=>{
-	Todo.remove({}).then(()=>done());
+	Todo.remove({}).then(()=>{
+		return Todo.insertMany(todos);
+	}).then(()=>done());
 });
 
 describe('Post/Todos', ()=>{
@@ -23,7 +28,7 @@ describe('Post/Todos', ()=>{
 					return done(err);
 				}
 
-				Todo.find().then((todos)=>{
+				Todo.find({text}).then((todos)=>{
 					expect(todos.length).toBe(1);
 					expect(todos[0].text).toBe(text);
 					done();
@@ -41,9 +46,22 @@ describe('Post/Todos', ()=>{
 				return done(err);
 			}
 			Todo.find().then((todos)=>{
-				expect(todos.length).toBe(0);
+				expect(todos.length).toBe(2);
 				done();
 			});
 		});
+	});
+});
+
+
+describe('GET/Todos', ()=>{
+	it('Should get all todos.', (done)=>{
+		request(app)
+		.get('/todos')
+		.expect(200)
+		.expect((res)=>{
+			expect(res.body.todos.length).toBe(2);
+		})
+		.end(done);
 	});
 });
